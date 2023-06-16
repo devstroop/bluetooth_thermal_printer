c38182476afa463fa5096842ffa51af23598fbff
akash <aksbju@gmail.com>
2023-06-16 22:17:09 +0530

    First commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.h>)
+#import <flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.h>
+#else
+@import flutter_bluetooth_basic;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterBluetoothBasicPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBluetoothBasicPlugin"]];
+}
+
+@end
---
+++ lib/bluetooth_thermal_printer.dart
@@ -0,0 +1,5 @@
+library bluetooth_thermal_printer;
+
+export './enums/print_result.type.dart';
+export './models/bluetooth_printer.model.dart';
+export './services/bluetooth_printer.service.dart';
